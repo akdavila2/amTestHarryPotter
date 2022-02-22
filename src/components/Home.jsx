006151cca91bc0f018f47761765c2f2ab9086a4a
akdavila2 <anakarina.daviladavila@gmail.com>
2022-02-21 21:29:57 -0600

    vista mobile de home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,22 @@ import iconFavorite from "../assets/icons-save.png";
 import iconAdd from "../assets/User_fill_add.png";
 import letters from "../assets/letters.png";
 import profilePic from "../assets/prfile pic.png";
-import iconFavCard from "../assets/Rectangle.png"
+import iconFavCard from "../assets/Rectangle.png";
 
 const Home = () => {
   return (
     <div className="home-container">
+      <section className="action-header">
+        <button className="btn-header">
+          FAVORITOS
+          <img src={iconFavorite} alt="favorito" />
+        </button>
+        <button className="btn-header">
+          Agregar
+          <img src={iconAdd} alt="agregar" />
+        </button>
+      </section>
       <header>
-        <section className="action-header">
-          <button className="btn-header">
-            FAVORITOS
-            <img src={iconFavorite} alt="favorito" />
-          </button>
-          <button className="btn-header">
-            Agregar
-            <img src={iconAdd} alt="agregar" />
-          </button>
-        </section>
-
         <section className="titles">
           <img src={letters} alt="harry potter" />
           <p>Selecciona tu filtro</p>
@@ -37,32 +36,71 @@ const Home = () => {
 
           <div className="card-info">
             <ul>
-              <li>
-                <span>VIVO/ESTUDIANTE</span><img src={iconFavCard} alt="icono favorito"/>
-              </li>
-              <li>
-                <h2>Estudiante</h2>
-              </li>
-              <li>
-                <h5>Cumpleaños:</h5>
-                <span>31-07-1980</span>
-              </li>
-              <li>
-                <h5>Género:</h5>
-                <span>Male</span>
-              </li>
-              <li>
-                <h5>Color de ojos:</h5>
-                <span>Green</span>
-              </li>
-              <li>
-                <h5>Color de pelo:</h5>
-                <span>Black</span>
-              </li>
+              <section className="card-header">
+                <li className="state-student">
+                  <span>VIVO/ESTUDIANTE</span>
+                  <img src={iconFavCard} alt="icono favorito" />
+                </li>
+                <li>
+                  <h3>Estudiante</h3>
+                </li>
+              </section>
+              <section className="card-form">
+                <li className="info-container">
+                  <p className="clave">Cumpleaños:</p>
+                  <span>31-07-1980</span>
+                </li>
+                <li className="info-container">
+                  <p className="clave">Género:</p>
+                  <span>Male</span>
+                </li>
+                <li className="info-container">
+                  <p className="clave">Color de ojos:</p>
+                  <span>Green</span>
+                </li>
+                <li className="info-container">
+                  <p className="clave">Color de pelo:</p>
+                  <span>Black</span>
+                </li>
+              </section>
             </ul>
           </div>
         </div>
-       
+        <div className="content-card">
+          <div className="card-pic">
+            <img src={profilePic} alt="foto personaje" />
+          </div>
+
+          <div className="card-info">
+            <ul>
+              <li className="state-student">
+                <span>VIVO/ESTUDIANTE</span>
+                <img src={iconFavCard} alt="icono favorito" />
+              </li>
+              <li>
+                <h3>Estudiante</h3>
+              </li>
+              <section className="card-form">
+                <li className="info-container">
+                  <p className="clave">Cumpleaños:</p>
+                  <span>31-07-1980</span>
+                </li>
+                <li className="info-container">
+                  <p className="clave">Género:</p>
+                  <span>Male</span>
+                </li>
+                <li className="info-container">
+                  <p className="clave">Color de ojos:</p>
+                  <span>Green</span>
+                </li>
+                <li className="info-container">
+                  <p className="clave">Color de pelo:</p>
+                  <span>Black</span>
+                </li>
+              </section>
+            </ul>
+          </div>
+        </div>
       </section>
     </div>
   );
